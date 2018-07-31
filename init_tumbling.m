4869154a11546569d2777155c97a8d85ef529980
Abin Alex Pothen <abinalexpothen@gmail.com>
2018-07-31 16:03:42 -0400

    body-fixed equation fixed

--- init_tumbling.m
+++ init_tumbling.m
@@ -13,12 +13,9 @@ omega0 = [1 -1 0.5]';
 J = 10^7*diag([1.3626 1.5333 0.3848]);
 
 % initial quaternion
-q0 = axang2quat(axang)';
-% q0 = [0.8034 0.1600 0.4272 0.3828]';
+%q0 = axang2quat(axang)';
+q0 = [0.8034 0.1600 0.4272 0.3828]';
 
 % initial quaternion derivative 
-%qd0 = [-0.5*q0(2:4)'*omega0;
-%     0.5*(q0(1)*eye(3)*omega0+cross(q0(2:4),omega0))
-%         ];
-    
+qd0 = [0.0379 0.6999 -0.2503 -0.0927]';    
 %tumbling_sim_v2
